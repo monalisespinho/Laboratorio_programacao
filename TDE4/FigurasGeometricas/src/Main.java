000930eb6be98656f1c0de03f836f2e4433fdd76
monalisespinho <pinhomonalise@gmail.com>
2023-03-28 21:34:53 -0300

    Commit

--- TDE4/FigurasGeometricas/src/Main.java
+++ TDE4/FigurasGeometricas/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Quadrado quadrado = new Quadrado();
+        quadrado.lado = 5;
+        double resultado = quadrado.calcularArea();
+        System.out.println(resultado);
+        Circunferencia circunferencia = new Circunferencia();
+        circunferencia.raio = 5;
+        double resultadoC = circunferencia.calcularArea();
+        System.out.println(resultadoC);
+        Trapezio trapezio = new Trapezio();
+        trapezio.baseMenor = 2;
+        trapezio.baseMaior = 5;
+        trapezio.altura = 7;
+        double resultadoT = trapezio.calcularArea();
+        System.out.println(resultadoT);
+        Triangulo triangulo = new Triangulo();
+        triangulo.altura = 4;
+        triangulo.base = 3;
+        double resultadoTr = triangulo.calcularArea();
+        System.out.println(resultadoTr);
+
+
     }
 }
